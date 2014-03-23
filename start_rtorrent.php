defa6f88fe186358f87cec149ecf4d22c2bd4eeb
jonnyboy <jonnyboy@jbmatrix.net>
2014-03-22 23:01:53 -0400

    Update start_rtorrent.php

--- start_rtorrent.php
+++ start_rtorrent.php
@@ -1,7 +1,6 @@
 <?php
 // This script will start multiple rtorrent sessions and shutdown each session before terminating.
 
-
 // Start tmux server
 exec("tmux start-server");
 
@@ -11,14 +10,14 @@ $tmux_session = "rtorrent";
 //check if session exists
 $exec = exec("tmux list-session | grep $tmux_session", $session);
 if (count($session) !== 0) {
-	if (isset($argv[1]) && $argv[1] === "kill") {
+	if (isset($argv[1]) && ($argv[1] === "kill" || $argv[1] === "exit")) {
 		exec("tmux send-keys -t $tmux_session:0 C-q");
 		exec("tmux send-keys -t $tmux_session:1 C-q");
 		exec("tmux send-keys -t $tmux_session:2 C-q");
 		exec("tmux send-keys -t $tmux_session:3 C-q");
 		exec("tmux send-keys -t $tmux_session:4 C-q");
 		$dead = false;
-		echo "Waiting for rtorrent seesions to terminate.\n";
+		echo "Waiting for rtorrent sessions to terminate.\n";
 		while ($dead === false) {
 			if (shell_exec("tmux list-panes -t${tmux_session}:0 | grep -c dead") == 1) {
 				if (shell_exec("tmux list-panes -t${tmux_session}:1 | grep -c dead") == 1) {
@@ -32,17 +31,41 @@ if (count($session) !== 0) {
 				}
 			}
 			sleep(10);
+			echo ".";
 		}
-		echo "All rtorrent sessions terminates.\n";
-		exec("tmux kill-session -t $tmux_session");
+		echo "\nAll rtorrent sessions terminated.\n";
+		if (isset($argv[1]) && $argv[1] === "exit") {
+			echo "tmux session $tmux_session has been terminated.\n"
+			exec("tmux kill-session -t $tmux_session");
+		}
 	} else {
+		// restart rtorrent sessions if dead
+		if (shell_exec("tmux list-panes -t${tmux_session}:0 | grep -c dead") == 1) {
+			exec("tmux respawnp -t $tmux_session:0 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent.rc'");
+		}
+		if (shell_exec("tmux list-panes -t${tmux_session}:1 | grep -c dead") == 1) {
+			exec("tmux respawnp -t $tmux_session:1 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-1.rc'");
+		}
+		if (shell_exec("tmux list-panes -t${tmux_session}:2 | grep -c dead") == 1) {
+			exec("tmux respawnp -t $tmux_session:2 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-2.rc'");
+		}
+		if (shell_exec("tmux list-panes -t${tmux_session}:3 | grep -c dead") == 1) {
+			exec("tmux respawnp -t $tmux_session:3 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-3.rc'");
+		}
+		if (shell_exec("tmux list-panes -t${tmux_session}:4 | grep -c dead") == 1) {
+			exec("tmux respawnp -t $tmux_session:4 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-4.rc'");
+		}
 		exec("tmux a -t $tmux_session");
 	}
 } else {
 	if (isset($argv[1]) && $argv[1] === "kill") {
 		echo "There is no session to kill.\n";
 		sleep(2);
+	} elseif (isset($argv[1]) && $argv[1] === "exit") {
+		echo "There is no session to exit.\n";
+		sleep(2);
 	}
+
 	exec("tmux -f ~/.tmux.conf new-session -d -s $tmux_session -n rutorrent 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent.rc'");
 	exec("tmux new-window -t $tmux_session:1 -n rutorrent-1 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-1.rc'");
 	exec("tmux new-window -t $tmux_session:2 -n rutorrent-2 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-2.rc'");
