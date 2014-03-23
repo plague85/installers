d72337f60d68f6e650c55bb6253c0f160f1f4473
jonnyboy <jonnyboy@jbmatrix.net>
2014-03-23 14:19:10 -0400

    Update start_rtorrent.php

--- start_rtorrent.php
+++ start_rtorrent.php
@@ -93,8 +93,8 @@ if (count($session) !== 0) {
 	exec("tmux -f ~/.tmux.conf new-session -d -s $tmux_session -n rutorrent 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent.rc'");
 	exec("tmux new-window -t $tmux_session:1 -n rutorrent-1 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-1.rc'");
 	exec("tmux new-window -t $tmux_session:2 -n rutorrent-2 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-2.rc'");
-	exec("tmux new-window -t $tmux_session:3 -n myananomouse-1 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-3.rc'");
-	exec("tmux new-window -t $tmux_session:4 -n myananomouse-2 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-4.rc'");
+	exec("tmux new-window -t $tmux_session:3 -n rutorrent-3 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-3.rc'");
+	exec("tmux new-window -t $tmux_session:4 -n rutorrent-4 'rtorrent -n -o http_capath=~/certs -o import=~/.rtorrent-4.rc'");
 	exec("tmux new-window -t $tmux_session:5 -n htop 'htop'");
 	exec("tmux new-window -t $tmux_session:6 -n vnstat 'watch -n30 \"vnstat -u && vnstat -i eth0\"'");
 	exec("tmux selectp -t 0; tmux splitw -t $tmux_session:6 -h -p 50 'vnstat -l'");
